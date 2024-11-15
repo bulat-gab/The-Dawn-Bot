a4c6aa25f64880becc7a846dab9fdbe03645ec9b
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-11-15 15:57:16 +0700

    add fabfile for deploying

---
+++ fabfile.py
@@ -0,0 +1,59 @@
+import os
+import logging
+
+from fabric import Connection, task
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv("./.env")
+
+SSH_KEY_PATH = os.getenv("SSH_KEY_PATH")
+REMOTE_HOST = os.getenv("REMOTE_HOST")
+REMOTE_USER = os.getenv("REMOTE_USER")
+GITHUB = os.getenv("GITHUB")
+PROJECT_PATH = os.getenv("PROJECT_PATH")
+LOCAL_PATH = os.getenv("LOCAL_PATH")
+
+
+def init_connection() -> Connection:
+    conn = Connection(
+        host=REMOTE_HOST,
+        user=REMOTE_USER,
+        connect_kwargs={
+            "key_filename": SSH_KEY_PATH,
+        }
+    )
+
+    return conn
+
+def upload_file(c: Connection, file_name: str):
+    local_folder = os.path.join(LOCAL_PATH, file_name)
+    remote_folder = PROJECT_PATH + "/" + file_name
+
+    result = c.put(local_folder, remote_folder)
+    logger.info("Uploaded {0.local} to {0.remote}".format(result))
+
+def is_repo_exist(c: Connection) -> bool:
+    result = c.run(f"if [ -d {PROJECT_PATH} ]; then echo 'exists'; else echo 'not found'; fi", hide=True)
+    is_exist = result.stdout.strip() == "exists"
+    if is_exist:
+        return True
+    
+    logger.info(f"Repository '{PROJECT_PATH}' does not exist. Cloning... ")
+    c.run(f"git clone {GITHUB}")
+    return False
+
+@task
+def deploy(c: Connection):
+    try:
+        c = init_connection()
+        c.run(f"cd {PROJECT_PATH} && git fetch origin main && git reset --hard origin/main")
+
+        # upload_file(c, "database/database.sqlite3") # Use with caution. Local database might contain expired auth tokens.
+        upload_file(c, "do_not_commit.config/settings.yaml")
+        upload_file(c, "do_not_commit.config/data/farm.txt")
+        c.run(f"cd {PROJECT_PATH} && docker compose up -d --build")
+    except Exception as e:
+        logger.error(f"Deploy has failed. {e}")
