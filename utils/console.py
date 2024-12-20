a3fba5d3d7edab1e0152661075d5e948523090eb
bulat-gab <gabdrakhmanov.br@gmail.com>
2024-12-20 22:28:30 +0300

    update logger

--- utils/console.py
+++ utils/console.py
@@ -1,19 +1,21 @@
 import sys
 import urllib3
 
-from loguru import logger
+from utils.loguru_logger import logger
 
 
 def setup():
+    level = "INFO"
+
     urllib3.disable_warnings()
     logger.remove()
     logger.add(
         sys.stdout,
+        level=level,
         colorize=True,
         format="<light-cyan>{time:HH:mm:ss}</light-cyan> | <level> {level: <8}</level> | - <white>{"
         "message}</white>",
     )
     logger.add("./logs/logs.log", rotation="1 day", retention="7 days")
-
 
 
