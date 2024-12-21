6def743058b0cd6a0b153aa85157d0ccd6891565
bulat-gab <gabdrakhmanov.br@gmail.com>
2024-12-21 09:54:14 +0300

    change pm2 name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ const isWindows = process.platform === "win32";
 module.exports = {
   apps: [
     {
-      name: "dawn_bot",
+      name: "dawn",
       script: "run.py",
       interpreter: isWindows ? "venv\\Scripts\\pythonw.exe" : "venv/bin/python",
       ignore_watch: ["deploy", "\\.git", "*.log"],
